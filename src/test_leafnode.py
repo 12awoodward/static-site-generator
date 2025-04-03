63166ed36c4294a63f54cee124507e9e3d2251f0
12awoodward <203260994+12awoodward@users.noreply.github.com>
2025-04-03 12:00:41 +0100

    added leafnode & tests

---
+++ src/test_leafnode.py
@@ -0,0 +1,24 @@
+import unittest
+
+from leafnode import LeafNode
+
+class TestLeafNode(unittest.TestCase):
+    def test_to_html(self):
+        node = LeafNode("p","text")
+        txt = '<p>text</p>'
+        self.assertEqual(node.to_html(), txt)
+    
+    def test_to_html2(self):
+        node = LeafNode(None,"text")
+        txt = 'text'
+        self.assertEqual(node.to_html(), txt)
+
+    def test_to_html3(self):
+        node = LeafNode("a","text", {"href":"/link/"})
+        txt = '<a href="/link/">text</a>'
+        self.assertEqual(node.to_html(), txt)
+
+        
+
+if __name__ == "__main__":
+    unittest.main()
