73592240833ae6eee3411512f00bda91f10ae966
12awoodward <203260994+12awoodward@users.noreply.github.com>
2025-04-07 11:59:57 +0100

    added block to block type func

--- src/block_functions.py
+++ src/block_functions.py
@@ -1,3 +1,48 @@
+from enum import Enum
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph block"
+    HEADING = "heading block"
+    CODE = "code block"
+    QUOTE = "quote block"
+    UNORDERED_LIST = "unordered list block"
+    ORDERED_LIST = "ordered list block"
+
+def block_to_block_type(block):
+    lines = block.split("\n")
+
+    if len(lines) == 1:
+        line_start = lines[0].split()[0]
+        if len(line_start) == line_start.count("#") and len(line_start) <= 6 and len(line_start) >= 1:
+            return BlockType.HEADING
+    
+    if lines[0][:3] == "```" and lines[-1][-3:] == "```":
+        return BlockType.CODE
+    
+    is_quote = True
+    is_o_list = True
+    is_u_list = True
+    count = 1
+    for line in lines:
+        if not (is_quote or is_o_list or is_u_list):
+            break
+        if line[0] != ">":
+            is_quote = False
+        start = line.split()[0]
+        if start != "-":
+            is_u_list = False
+        if start != str(count) + ".":
+            is_o_list = False
+        count += 1
+    
+    if is_quote:
+        return BlockType.QUOTE
+    if is_o_list:
+        return BlockType.ORDERED_LIST
+    if is_u_list:
+        return BlockType.UNORDERED_LIST
+
+    return BlockType.PARAGRAPH
 
 
 def markdown_to_blocks(markdown):
--- src/test_block_functions.py
+++ src/test_block_functions.py
@@ -3,6 +3,31 @@ import unittest
 from block_functions import *
 
 class TestInlineFunctions(unittest.TestCase):
+    # PARAGRAPH HEADING CODE QUOTE UNORDERED_LIST ORDERED_LIST
+    def test_block_to_block_type_paragraph(self):
+        block = "some text\nin a paragraph"
+        self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
+
+    def test_block_to_block_type_heading(self):
+        block = "### h3 heading"
+        self.assertEqual(block_to_block_type(block), BlockType.HEADING)
+
+    def test_block_to_block_type_code(self):
+        block = "```code\nin a\nblock```"
+        self.assertEqual(block_to_block_type(block), BlockType.CODE)
+
+    def test_block_to_block_type_quote(self):
+        block = ">quote\n>block"
+        self.assertEqual(block_to_block_type(block), BlockType.QUOTE)
+
+    def test_block_to_block_type_u_list(self):
+        block = "- items\n- in\n- a\n- list"
+        self.assertEqual(block_to_block_type(block), BlockType.UNORDERED_LIST)
+
+    def test_block_to_block_type_o_list(self):
+        block = "1. items\n2. in\n3. order"
+        self.assertEqual(block_to_block_type(block), BlockType.ORDERED_LIST)
+
     def test_markdown_to_blocks(self):
         md = "This is **bolded** paragraph\n\nThis is another paragraph with _italic_ text and `code` here\nThis is the same paragraph on a new line\n\n- This is a list\n- with items"
         blocks = markdown_to_blocks(md)
