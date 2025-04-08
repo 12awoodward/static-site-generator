7d347a03237cd70fafe9c0af39a5de2ae3f9abdf
12awoodward <203260994+12awoodward@users.noreply.github.com>
2025-04-08 16:16:48 +0100

    added generate page function

--- src/main.py
+++ src/main.py
@@ -4,9 +4,33 @@ from leafnode import *
 from parentnode import *
 from inline_functions import *
 from block_functions import *
+from markdown_to_html import *
 
 import os
 import shutil
+
+def generate_page(from_path, template_path, dest_path):
+    print(f"Generating page from {from_path} to {dest_path} using {template_path}")
+    markdown = read_file(from_path)
+    template = read_file(template_path)
+    html = markdown_to_html_node(markdown).to_html()
+    title = extract_title(markdown)
+
+    html_page = template.replace("{{ Title }}", title).replace("{{ Content }}", html)
+    write_file(dest_path, html_page)
+
+
+def read_file(path):
+    with open(path, "r") as file:
+        return file.read()
+    
+def write_file(path, content):
+    file_dir = os.path.dirname(path)
+    if not os.path.exists:
+        os.makedirs(file_dir)
+    
+    with open(path, "w") as file:
+        file.write(content)
 
 def copy_files(origin, destination):
     if not(os.path.exists(origin) and os.path.exists(destination)):
@@ -33,5 +57,7 @@ def extract_title(markdown):
 
 def main():
     copy_files("static", "public")
+
+    generate_page("content/index.md", "template.html", "public/index.html")
 
 main()
